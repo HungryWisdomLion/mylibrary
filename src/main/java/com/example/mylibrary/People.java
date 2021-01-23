7d3f555e917a60898d7a786096bc5d29fb678f89
yishaqi <2543671857@qq.com>
2021-01-23 11:13:35 +0800

    people Signed-off-by: yishaqi <2543671857@qq.com>

---
+++ src/main/java/com/example/mylibrary/People.java
@@ -0,0 +1,22 @@
+package com.example.mylibrary;
+
+public class People {
+    private String name;
+    private String unit;
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
